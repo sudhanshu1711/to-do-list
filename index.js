d01f8c77d49c475d6a9889976573d0b33403efd2
saroj kumar <sudhanshu2679@gmail.com>
2024-07-09 09:39:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+
+    let heading = document.getElementById('heading');
+    let box = document.getElementById('box');
+    let lists = document.getElementById('lists');
+    let main = document.querySelector('.main');
+
+    box.addEventListener('keydown', function(e) {
+        if (e.key === 'Enter') {
+            addGoal();
+        }
+    });
+
+    function addGoal() {
+        let goal = box.value.trim(); 
+        if (goal !== "") {
+            let mission = document.createElement('li');
+            mission.textContent = goal;
+            lists.appendChild(mission);
+            box.value = ""; 
+            updateMainText();
+        }
+    }
+
+    lists.addEventListener('dblclick', function(e) {
+        if (e.target.tagName === 'LI') 
+            e.target.remove();
+            if(lists.childElementCount==0){
+            main.innerHTML='<h1>YAY!<br> ALL WORK DONE';
+        }
+    });
+
+
+
+
+
